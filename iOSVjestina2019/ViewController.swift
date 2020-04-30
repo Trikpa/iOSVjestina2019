b04bc6a1ba19d09f3f5625c1c73e81b905da2e1b
Patrik Badanjak <pbadanjak@gmail.com>
2020-04-30 22:48:01 +0200

    initial commit

---
+++ iOSVjestina2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSVjestina2019
+//
+//  Created by Five on 30/04/2020.
+//  Copyright © 2020 Algebra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
