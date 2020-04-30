21e07e069210aa636d3e08271113b1a57666ecc2
Patrik Badanjak <pbadanjak@gmail.com>
2020-05-01 00:08:52 +0200

    Added a button, label and bottom navigation bar

--- iOSVjestina2019/ViewController.swift
+++ iOSVjestina2019/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundGradientView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        gradientLayer.colors = [UIColor(red: 196/255, green: 15/255, blue: 97/255, alpha: 1).cgColor, UIColor(red: 226/255, green: 114/255, blue: 16/255, alpha: 1).cgColor]
+        gradientLayer.shouldRasterize = true
+        
+        backgroundGradientView.layer.addSublayer(gradientLayer)
+        
+        gradientLayer.startPoint = CGPoint(x: 1, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.6, y: 1)
     }
-
 
 }
 
